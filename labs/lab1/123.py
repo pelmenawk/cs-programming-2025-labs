b712d06610714fcf234f64e2a3b98c47f4392827
pelmenawk <guzkirill555@mail.ru>
2025-09-29 10:37:17 +1000

    Update 123.py

--- labs/lab1/123.py
+++ labs/lab1/123.py
@@ -1,54 +1,55 @@
-#1
-# x = 28
-# y= 3,1415
-# z = "Пайтон"
-# w= False
-# 2
-# a= "Кирилл"
-# b= '18'
-# print(a,b)
-#3
-# a=342
-# b= 56.2
-# c='43'
-# d=(a+b + int(c))
-# print(d)
-#4
-# a=3
-# b=8
-# print((a+4*b)*(a-3*b)+a**2)
-#5
-# a=int(input("сторона1:"))
-# b=int(input("сторона2:"))
-# print((2*a+2*b),(a*b))
-#6
-# print(' *   *   *')
-# print('  * * * *')
-# print('   *   *')
-#7
-# x=15
-# y=10
-# print(f"x*y={x*y};x+y={x+y};x//y={x//y};x-y={x-y};x%y={x%y};x^y={x**y};x/y={x/y}")
-# print(f"x=y={x==y};x!=y={x!=y};x>y={x>y};x<y={x<y};x<=y={x<=y};x>=y={x>=y}")
-#8
-# a="Кирилл"
-# b="18"
-# print(f"Меня зовут {a},мне {b} лет.")
-#9
-# a="Съешь еще "
-# b="этих мягких "
-# c="французских булок,"
-# d="да выпей чаю"
-# print(a+b+c+d)
-# 10
-# print("Да! Нет!"*4)
-#11
-# a=input()
-# x1,x2,x3=a.split(",")
-# print(f"Результат вычисления:{(int(x1)+int(x3))//int(x2)}")
-#12
-# a=input()
-# print(a[0:4])
-# print(a[-2:])
-# print(a[3:7])
-# print(a[::-1])
+def num1():
+    x = 28
+    y= 3,1415
+    z = "Пайтон"
+    w= False
+def num2():
+    a= "Кирилл"
+    b= '18'
+    print(a,b)
+def num3():
+    a=342
+    b= 56.2
+    c='43'
+    d=(a+b + int(c))
+    print(d)
+def num4():
+    a=3
+    b=8
+    print((a+4*b)*(a-3*b)+a**2)
+def num5():
+    a=int(input("сторона1:"))
+    b=int(input("сторона2:"))
+    print((2*a+2*b),(a*b))
+def num6():
+    print(' *   *   *')
+    print('  * * * *')
+    print('   *   *')
+def num7():
+    x=15
+    y=10
+    print(f"x*y={x*y};x+y={x+y};x//y={x//y};x-y={x-y};x%y={x%y};x^y={x**y};x/y={x/y}")
+    print(f"x=y={x==y};x!=y={x!=y};x>y={x>y};x<y={x<y};x<=y={x<=y};x>=y={x>=y}")
+def num8():
+    a="Кирилл"
+    b="18"
+    print(f"Меня зовут {a},мне {b} лет.")
+def num9():
+    a="Съешь еще "
+    b="этих мягких "
+    c="французских булок,"
+    d="да выпей чаю"
+    print(a+b+c+d)
+def num10():
+    print("Да! Нет!"*4)
+def num11():
+    a=input()
+    x1,x2,x3=a.split(",")
+    print(f"Результат вычисления:{(int(x1)+int(x3))//int(x2)}")
+def num12():
+    a=input()
+    print(a[0:4])
+    print(a[-2:])
+    print(a[3:7])
+    print(a[::-1])
+num2()
