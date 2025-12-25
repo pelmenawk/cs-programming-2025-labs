3d88b50b5556e9e338efe2921d32dc85dbd3747d
pelmenawk <guzkirill555@mail.ru>
2025-12-26 00:34:08 +1000

    Update num2.py

--- labs/lab6/num2.py
+++ labs/lab6/num2.py
@@ -1,23 +1,22 @@
-vklad=input("Введите сумму вклада(в рублях) и срок(в годах):").split(" ")
+vklad=input("Введите сумму вклада(в рублях) и срок(в годах):").split()
 a=int(vklad[0])
 n=int(vklad[-1])
 
 def income(x,y):
-    vklad1=0
-    shet=x-30000
-    while shet>10000 and vklad1<0.05:
-        shet-=10000
-        vklad1+=0.003
-    srokstav=0
-    if y <=3:
-        srokstav==0.03
-    if 4<=y<=6:
-        srokstav==0.05
-    if y>6:
-        srokstav==0.02
-    vsestav=vklad1+srokstav
-    for i in range(n):
-        x1=x*(1+vsestav)
-    x2=x1-x
-    print(x2)
+    for i in range(1, y + 1):
+        vklad1=0
+        shet=x
+        while shet>9999 and vklad1<0.05:
+            shet-=10000
+            vklad1+=0.003
+        vklad1 = round(vklad1,4)
+        if i <=3:
+            vklad1+=0.03
+        if 4<=i<=6:
+            vklad1+=0.05
+        if i>6:
+            vklad1+=0.02
+        x1 = round(x * (1+vklad1),2)
+        x = x1
+    print(round(x1 - a,2))
 income(a,n)
