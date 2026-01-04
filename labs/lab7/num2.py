8d73167a23411c2c9b9d7f941469085f6207feae
pelmenawk <guzkirill555@mail.ru>
2026-01-04 15:21:54 +1000

    Create num2.py

---
+++ labs/lab7/num2.py
@@ -0,0 +1,8 @@
+staff_shifts = [
+    {"name": "Dr. Shaw", "shift_cost": 120, "shifts": 15},
+    {"name": "Agent Torres", "shift_cost": 90, "shifts": 22},
+    {"name": "Researcher Hall", "shift_cost": 150, "shifts": 10}
+]
+a=map(lambda x:x["shift_cost"]*x["shifts"],staff_shifts)
+b=max(a)
+print(b)
