21fc2978deabac74b501666bb661329f7e41346a
pelmenawk <guzkirill555@mail.ru>
2026-01-04 15:25:49 +1000

    Delete num10.py

--- labs/lab7/num10.py
+++
@@ -1,8 +0,0 @@
-evaluations = [
-    {"name": "Agent Cole", "score": 78},
-    {"name": "Dr. Weiss", "score": 92},
-    {"name": "Technician Moore", "score": 61},
-    {"name": "Researcher Lin", "score": 88}
-]
-a=max(evaluations,key=lambda x:x["score"])
-print(a)
